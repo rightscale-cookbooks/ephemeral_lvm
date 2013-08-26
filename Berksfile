412ff53673c44fd75ac96363b0e353c1c65de64d
Douglas Thrift <douglas.thrift@rightscale.com>
2013-08-26 15:14:38 -0700

    acu114901 - Bring in cookbook dependencies.

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 site :opscode
 
 metadata
+
+cookbook "lvm", github: "arangamani-cookbooks/lvm", tag: "0.8.11"
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures ephemeral'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
+depends 'xfs', '~> 1.1.0'
+depends 'lvm'
