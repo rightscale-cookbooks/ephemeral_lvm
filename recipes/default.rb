fec77265ed9d271380c6307b1a624e3925e662f7
Lopaka Delp <lopaka@rightscale.com>
2015-04-13 12:55:40 -0700

    ST-1 Attach and install updated di-ruby-lvm-attrib gem.  Bump version to 1.0.10.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,19 @@
 # limitations under the License.
 #
 
+# Due to issue https://github.com/gregsymons/di-ruby-lvm-attrib/issues/22
+# an update for LVM 2.0.2.115 was needed to work on RHEL/CentOS 7.1.
+# Once pull request is merged, issue closed, and rubygems udpated,
+# this section and the source file can be removed.
+cookbook_file '/tmp/di-ruby-lvm-attrib-0.0.17.gem' do
+  source "di-ruby-lvm-attrib-0.0.17.gem"
+  action :nothing
+end.run_action(:create)
+chef_gem 'di-ruby-lvm-attrib' do
+  source '/tmp/di-ruby-lvm-attrib-0.0.17.gem'
+  action :install
+end
+
 # Include the lvm::default recipe which sets up the resources/providers for lvm
 #
 include_recipe "lvm"
